d3bacbd97e9d837c6e77f46727c870de0b73f799
gauravbhatkar678 <gauravbhatkar678@gmail.com>
2025-04-24 17:20:54 +0530

    javascript conditions

---
+++ java script/video56/index.js
@@ -0,0 +1,26 @@
+// let age = 18;
+// if (age >= 18) {
+//   console.log("You can drive");
+// }
+
+// let age = 10;
+// if (age >= 18) {
+//   console.log("you can drive");
+// } else {
+//   console.log("you are minor, you can not drive");
+// }
+
+// let a = 5;
+// let b = 6;
+// a > b ? console.log("true") : console.log("false");
+
+a = 10;
+b = 20;
+// while (a <= b) {
+//   console.log("value of a is : ", a);
+//   ++a;
+// }
+
+do {
+  console.log("value of a is : ", a);
+} while (a > b);
