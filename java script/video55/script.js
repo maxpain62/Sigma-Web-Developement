99c212e877b90f044c753fdbed68cab2126d24a1
gauravbhatkar678 <gauravbhatkar678@gmail.com>
2025-04-24 17:20:11 +0530

    javascript variables

---
+++ java script/video55/script.js
@@ -0,0 +1,29 @@
+// var a = 5;
+// console.log(a);
+let a = 7;
+console.log(a);
+{
+  let a = 8;
+  console.log("a inside block " + a);
+}
+
+let x = "Harry bhai";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let o = {
+  name: "Harry",
+  "job code": 5600,
+  is_handsome: true,
+};
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o);
