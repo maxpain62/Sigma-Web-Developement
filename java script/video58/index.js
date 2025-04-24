f9bba66ecb2aaba54c4f29a4e9a5a661d83633ef
gauravbhatkar678 <gauravbhatkar678@gmail.com>
2025-04-24 17:21:49 +0530

    javascript functions

---
+++ java script/video58/index.js
@@ -0,0 +1,20 @@
+function addition(a, b) {
+  console.log(a + b);
+}
+
+addition(1, 2);
+
+function substraction(a, b) {
+  console.log(a - b);
+}
+substraction(1, 2);
+
+function multiplication(a, b) {
+  console.log(a * b);
+}
+multiplication(2, 2);
+
+function division(a, b) {
+  console.log(a / b);
+}
+division(100, 5);
